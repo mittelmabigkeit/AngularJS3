be9549de0fda74828f731a26c3ade79069a6732c
mittelmabigkeit <comp422012016@mail.ru>
2016-10-06 21:02:42 +0300

    six

--- six/main.js
+++ six/main.js
@@ -3,7 +3,7 @@ var app = angular.module("SampleApp", []);
 		app.controller("SampleAppCtrl", function ($scope) {
 
 			$scope.clickHandler = function () {
-				$scope.classNames = "test-div lightGreen";
+				$scope.classNames = ["test-div", "lightGreen"];
 			}
 
 		});
