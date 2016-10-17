1f5d5245e187daaed1c54b8fc4fa4dd384961ad7
mittelmabigkeit <comp422012016@mail.ru>
2016-10-17 16:55:34 +0300

    thirteen

---
+++ thirteen/main.js
@@ -0,0 +1,9 @@
+angular.module("sampleApp",[])
+.controller("defaultCtrl", function ($scope) {
+	$scope.tasks = [
+		{ action: "Task 1", complete: false },
+		{ action: "Task 2", complete: true },
+		{ action: "Task 3", complete: false },
+		{ action: "Task 4", complete: true }
+		];
+});
